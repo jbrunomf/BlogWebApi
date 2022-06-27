66bf5a4cd6659636f67157a6c64567a0dfb393f4
João Bruno Moura Fernandes <jbrunomf@outlook.com>
2022-06-26 20:35:08 -0400

    refatorando os métodos

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using BlogWebApi.Data;
+using BlogWebApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -27,6 +28,59 @@ namespace BlogWebApi.Controllers
             }
 
             return Ok(category);
+        }
+
+        [HttpPost("v1/categories")]
+        public async Task<IActionResult> Post(
+            [FromBody] Category model,
+            [FromServices] BlogDataContext context)
+        {
+            await context.Categories.AddAsync(model);
+            await context.SaveChangesAsync();
+
+            return Created($"v1/categories/{model.Id}", model);
+        }
+
+        [HttpPut("v1/categories/{id:int}")]
+        public async Task<IActionResult> Put(
+            [FromRoute] int id,
+            [FromBody] Category model,
+            [FromServices] BlogDataContext context)
+        {
+            var category = context.Categories.SingleOrDefault(x => x.Id == id);
+
+            if (category is null)
+            {
+                return NotFound();
+            }
+
+            category.Name = model.Name;
+            category.Posts = model.Posts;
+            category.Slug = model.Slug;
+
+            context.Categories.Update(category);
+            await context.SaveChangesAsync();
+
+            return Ok(category);
+        }
+
+
+        [HttpDelete("v1/categories/{id}")]
+        public async Task<IActionResult> DeleteAsync(
+            [FromRoute] int id,
+            [FromServices] BlogDataContext context)
+        {
+            var category = await context.Categories.SingleOrDefaultAsync(x => x.Id == id);
+
+            if (category is null)
+            {
+                return NotFound();
+            }
+
+            context.Categories.Remove(category);
+            await context.SaveChangesAsync();
+
+            return Ok();
         }
     }
 }
