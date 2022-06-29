f7f9aa43466b60369cae954252a894e9e2f27599
João Bruno Moura Fernandes <jbrunomf@outlook.com>
2022-06-28 21:27:13 -0400

    add: classe para fazer o handle do JWT

---
+++ Services/TokenService.cs
@@ -0,0 +1,19 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using BlogWebApi.Models;
+using Microsoft.IdentityModel.Tokens;
+
+namespace BlogWebApi.Services
+{
+    public class TokenService
+    {
+        public string GenerateToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler(); // Instanciando TokenHandler
+            var key = Encoding.ASCII.GetBytes(Configuration.JwtKey); // Pegando a Key
+            var tokenDescriptor = new SecurityTokenDescriptor(); // Criando especificacao do token
+            var token = tokenHandler.CreateToken(tokenDescriptor); // Criando o token
+            return tokenHandler.WriteToken(token); // Retornando o token com uma string.
+        }
+    }
+}
