5b0dee6167917a7e772484af22187877901ac4c8
João Bruno Moura Fernandes <jbrunomf@outlook.com>
2022-06-30 22:29:26 -0400

    add: métodos para login com verificação da hash (password)

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -63,7 +63,7 @@ namespace BlogWebApi.Controllers
 
 
         [HttpPost("v1/login")]
-        public IActionResult Login()
+        public IActionResult Log()
         {
             var tokenService = new TokenService();
             var token = tokenService.GenerateToken(null);
@@ -72,18 +72,39 @@ namespace BlogWebApi.Controllers
         }
 
 
-        [Authorize(Roles = "user")]
-        [HttpGet("v1/user")]
-        public IActionResult GetUser() => Ok(User.Identity.Name);
-        
-        [Authorize(Roles = "author")]
-        [HttpGet("v1/author")]
-        public IActionResult GetAuthor() => Ok(User.Identity.Name);
+        [HttpPost("v1/accounts/login")]
+        public async Task<IActionResult> Login(
+            [FromBody] LoginViewModel model,
+            [FromServices] BlogDataContext context,
+            [FromServices] TokenService tokenService)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new ResultViewModel<string>(ModelState.GetErrors()));
+            }
 
-        [Authorize(Roles = "admin")]
-        [HttpGet("v1/admin")]
-        public IActionResult GetAdmin() => Ok(User.Identity.Name);
+            var user = await context
+                .Users
+                .AsNoTracking()
+                .Include(x => x.Roles)
+                .FirstOrDefaultAsync(x => x.Email == model.Email);
 
+            if (user == null)
+                return StatusCode(401, new ResultViewModel<string>("Usuário ou senha inválidos"));
+
+            if (!PasswordHasher.Verify(user.PasswordHash, model.Password))
+                return StatusCode(401, new ResultViewModel<string>("Usuario ou senha inválidos"));
+
+            try
+            {
+                var token = tokenService.GenerateToken(user);
+                return Ok(new ResultViewModel<string>(token, null));
+            }
+            catch
+            {
+                return StatusCode(500, new ResultViewModel<string>("Falha interna do servidor."));
+            }
+        }
 
     }
 }
