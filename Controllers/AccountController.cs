49e07f012c754c1454050724809bd92587cb13fe
João Bruno Moura Fernandes <jbrunomf@outlook.com>
2022-06-30 21:06:51 -0400

    impl: hash de senhas

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,11 +1,15 @@
-﻿using BlogWebApi.Models;
+﻿using BlogWebApi.Data;
+using BlogWebApi.Extensions;
+using BlogWebApi.Models;
 using BlogWebApi.Services;
+using BlogWebApi.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SecureIdentity.Password;
 
 namespace BlogWebApi.Controllers
 {
-    [Authorize]
     [ApiController]
     public class AccountController : Controller
     {
@@ -17,6 +21,47 @@ namespace BlogWebApi.Controllers
 
 
         [AllowAnonymous]
+        [HttpPost("v1/accounts")]
+        public async Task<IActionResult> Post(
+            [FromBody] RegisterViewModel model,
+            [FromServices] BlogDataContext context)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new ResultViewModel<string>(ModelState.GetErrors()));
+                
+            }
+
+            var user = new User
+            {
+                Name = model.Name,
+                Email = model.Email,
+                Slug = model.Email.Replace("@", "-").Replace(".", "-")
+            };
+
+            var password = PasswordGenerator.Generate(25);
+            user.PasswordHash = PasswordHasher.Hash(password);
+
+            try
+            {
+                await context.Users.AddAsync(user);
+                await context.SaveChangesAsync();
+                return Ok(new ResultViewModel<dynamic>(new
+                {
+                    user = user.Email, password
+                }));
+            }
+            catch (DbUpdateException e)
+            {
+                return StatusCode(400, value: new ResultViewModel<string>("Erro."));
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+
         [HttpPost("v1/login")]
         public IActionResult Login()
         {
---
+++ ViewModels/RegisterViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogWebApi.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "E-mail é obrigatório")]
+        [EmailAddress(ErrorMessage = "O E-mail é inválido.")]
+        public string Email { get; set; }
+    }
+}
